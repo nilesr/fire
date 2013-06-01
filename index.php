b53dafe78e0e5a1d50afc4dfe10e0e8689e70594
Niles Rogoff <nilesrogoff@gmail.com>
2013-06-01 17:01:43 -0400

    Moved some stuff around

--- index.php
+++ index.php
@@ -4,28 +4,48 @@
 		<link rel="stylesheet" href="style.css">
 		<meta name="apple-mobile-web-app-capable" content="yes" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-		<script type="text/javascript" src="http://d3lp1msu2r81bx.cloudfront.net/kjs/js/lib/kinetic-v4.5.3.min.js"></script>
-		<script type="text/javascript">
-
-			function drawImage(imageObj)
-			{
-				var stage = new Kinetic.Stage({
-					container: "body",
-					width: screen.width,
-					height: screen.height
-				});
-				var layer = new Kinetic.Layer();
-				var thingy = new Kinetic.Image({
-					image: imageObj,
-					x: 0,
-					y: 0,
-					width: 25,
-					height: 25,
-					draggable: true
-				});
-				layer.add(thingy);
-				stage.add(layer);
+		<script src="http://d3lp1msu2r81bx.cloudfront.net/kjs/js/lib/kinetic-v4.5.3.min.js"></script>
+		<script type="text/javascript" defer="defer">
+			function writeMessage(messageLayer, message) {
+				var context = messageLayer.getContext();
+				messageLayer.clear();
+				context.font = '18pt Calibri';
+				context.fillStyle = 'black';
+				context.fillText(message, 10, 25);
 			}
+			var stage = new Kinetic.Stage({
+				container: 'container',
+				width: screen.width,
+				height: screen.heigth
+			});
+			var boxLayer = new Kinetic.Layer();
+			var messageLayer = new Kinetic.Layer();
+			var rectX = 0;
+			var rectY = 0;
+
+			var box = new Kinetic.Rect({
+				x: rectX,
+				y: rectY,
+				width: 100,
+				height: 50,
+				fill: '#00D2FF',
+				stroke: 'black',
+				strokeWidth: 4,
+				draggable: true
+			});
+
+			// write out drag and drop events
+			box.on('dragstart', function() {
+				writeMessage(messageLayer, 'dragstart');
+			});
+			box.on('dragend', function() {
+				writeMessage(messageLayer, 'dragend');
+			});
+
+			boxLayer.add(box);
+
+			stage.add(messageLayer);
+			stage.add(boxLayer);
 			function DrawBuildings()
 			{
 				var floors = prompt("How many floors?");
@@ -42,6 +62,12 @@
 					canvas.height = 50 * floors;
 					canvas.style.height = 50 * floors + "px";
 					verticalpixelspace = 50;
+				}
+				if (horizontalpixelspace < 100)
+				{
+					canvas.width = 100 * rows;
+					canvas.style.width = 100 * rows + "px";
+					horizontalpixelspace = 100;
 				}
 				var tempfloors = floors
 				while (tempfloors > -1)
@@ -73,17 +99,11 @@
 					tempfloors = tempfloors + 1;
 				}
 			}
-			function NewThingy() {
-				var imageObj = new Image();
-				imageObj.onload = function() {
-					drawImage(this);
-				};
-				imageObj.src = 'green.png';
-			}
 		</script>
 		<title><? echo shell_exec('date "+%A, %h %d %G, %r"'); ?></title>
 	</head>
 	<body id="body">
+		<div id="container"></div>
 		<noscript>
 			<h1>Sorry, but you need Javascript for this program to work</h1>
 		</noscript>
@@ -92,7 +112,5 @@
 		<canvas id="maincanvas">
 			<p>Your browser does not support the canvas element, sorry</p>
 		</canvas>
-		<span id="box">
-		</span>
 	</body>
 </html>
